af404a5843d6082dcc75a44055dee28fa0166985
g-kitiashvili <qitiashvili13@gmail.com>
2023-06-15 03:21:36 +0400

    first commit

---
+++ src/main/java/com/example/Thumbnote/ThumbnoteApplication.java
@@ -0,0 +1,13 @@
+package com.example.Thumbnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThumbnoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThumbnoteApplication.class, args);
+	}
+
+}
