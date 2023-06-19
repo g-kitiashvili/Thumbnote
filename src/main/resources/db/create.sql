d1c87e6035b339e1b7c2c6bcd360ca1ddf930687
g-kitiashvili <qitiashvili13@gmail.com>
2023-06-20 03:54:04 +0400

    created tables i think i will need

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -1,9 +1,53 @@
 create database thumbnote_db;
 use thumbnote_db;
+SET FOREIGN_KEY_CHECKS=0;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS notes;
+DROP TABLE IF EXISTS notebooks;
+DROP TABLE IF EXISTS tags;
+SET FOREIGN_KEY_CHECKS=1;
+
+
 CREATE TABLE users (
-                       user_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                       username VARCHAR(255) NOT NULL,
+                       user_id BIGINT AUTO_INCREMENT PRIMARY KEY  ,
+                       username VARCHAR(255) NOT NULL unique,
                        password_hash VARCHAR(255) NOT NULL,
                        email VARCHAR(255) NOT NULL
 );
-select * from users
+
+create table notes (
+        note_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+        user_id BIGINT NOT NULL,
+        upload_date timestamp,
+        last_update date,
+        last_access_date timestamp,
+        note_name varchar(255),
+        note mediumtext,
+        FOREIGN KEY (user_id) REFERENCES users(user_id),
+        unique(note_name,user_id)
+#         unique key(note_id,user_id)
+
+);
+
+create table tags(
+        tag_id BIGINT auto_increment primary key ,
+        note_id BIGINT,
+        tag_name VARCHAR(255) unique,
+        FOREIGN KEY (note_id) REFERENCES notes (note_id),
+        unique key(tag_name,note_id)
+
+
+);
+
+create table notebooks(
+    notebook_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    notebook_name VARCHAR(255),
+    description tinytext,
+    user_id bigINT,
+    note_id bigINT,
+    FOREIGN KEY (note_id) REFERENCES Notes(note_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    unique key(note_id,notebook_id)
+
+)
+
